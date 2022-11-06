90d8c86d2cec108446989b1e2eb52cdebde3f705
mohamed-elrefai <mohamed.mostafa0699@gmail.com>
2022-11-06 12:18:20 +0200

    chore: handler & serverless

--- API/handler.js
+++ API/handler.js
@@ -1,15 +1,8 @@
-"use strict";
+const awsServerlessExpress = require('aws-serverless-express');
+const app = require('./src/index');
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v3.0! Your function executed successfully!",
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
-};
+const server = awsServerlessExpress.createServer(app);
+
+exports.handler = (event, context) => {
+  return awsServerlessExpress.proxy(server, event, context);
+}
