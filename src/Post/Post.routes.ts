02631db23be2a0289207c4b8134d705635727b7c
mohamed-elrefai <mohamed.mostafa0699@gmail.com>
2022-11-13 05:24:56 +0200

    chore: Post

--- src/Post/Post.routes.ts
+++ src/Post/Post.routes.ts
@@ -1,3 +1,4 @@
+import { PostController } from './Post.service';
 import { ControllerInterFaces } from "../Types/Controller/Controller";
 import verifyToken from "../Middleware/Auth.middleware";
 import { Router } from 'express';
@@ -6,12 +7,16 @@ import { Router } from 'express';
 export class UserRoutes implements ControllerInterFaces {
     public path = '/api';
     public router = Router();
-
+    private post = new PostController()
     constructor() {
         this.initializeRoutes();
     }
     private initializeRoutes(){
-        this.router.get(`${this.path}/`, verifyToken,)
-
+        this.router.get(`${this.path}/getallpost`, verifyToken, this.post.GetPost)
+        this.router.get(`${this.path}/getpost`, verifyToken, this.post.GetSinglePost)
+        this.router.post(`${this.path}/create/post`, verifyToken, this.post.createPost)
+        this.router.delete(`${this.path}/delete/post`, verifyToken, this.post.DeletePost)
+        this.router.put(`${this.path}/update/post`, verifyToken, this.post.UpdatelePost)
+        this.router.post(`${this.path}/like/post`, verifyToken, this.post.LikeePost)
     }
 }
--- src/Post/Post.service.ts
+++ src/Post/Post.service.ts
@@ -2,9 +2,80 @@ import { PostModel } from "../Models/Post/Post.model";
 import { Request, Response, NextFunction } from "express";
 
 export class PostController {
-    public async createPost (req: Request, res: Response, next: NextFunction) {
+    public async createPost (req: Request | any, res: Response, next: NextFunction) {
         try{
-            
+            const data = req.user.id;
+            const post = await new PostModel(data);
+            await post.save();
+
+            res.status(200).json('Data saved successfully')
+            next();
+        }catch(err){
+            res.status(500).json(err)
+        }
+    }
+
+    public async GetPost (_req: Request | any, res: Response, next: NextFunction) {
+        try{
+            const post = await PostModel.find();
+            res.status(200).json(post)
+            next();
+        }catch(err){
+            res.status(500).json(err)
+        }
+    }
+    
+    public async GetSinglePost (req: Request | any, res: Response, next: NextFunction) {
+        try{
+            const post = await PostModel.findById(req.params.id);
+            res.status(200).json(post)
+            next();
+        }catch(err){
+            res.status(500).json(err)
+        }
+    }
+
+    public async UpdatelePost (req: Request | any, res: Response, next: NextFunction) {
+        try{
+            await PostModel.findByIdAndUpdate(req.params.id, {
+                $set: req.body
+            });
+            res.status(200).json("Post updated successfully")
+            next();
+        }catch(err){
+            res.status(500).json(err)
+        }
+    }
+
+    public async DeletePost (req: Request | any, res: Response, next: NextFunction) {
+        try{
+            await PostModel.findByIdAndDelete(req.params.id);
+            res.status(200).json(" Post deleted successfully")
+            next();
+        }catch(err){
+            res.status(500).json(err)
+        }
+    }
+
+    public async LikeePost (req: Request | any, res: Response, next: NextFunction) {
+        try{
+            const postID = await PostModel.findById(req.params.id);
+            if(!postID?.likes.includes(req.user.id)){
+                await postID?.updateOne({
+                    $push: {
+                        likes: req.user.id,
+                    }
+                })
+                res.status(200).json("The post has been liked");
+            }else{
+                await postID?.updateOne({
+                    $pull: {
+                        likes: req.user.id,
+                    }
+                })
+                res.status(200).json("The post has been disliked");
+            } 
+            next();
         }catch(err){
             res.status(500).json(err)
         }
